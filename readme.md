4e50f7b7f877e8fc89fe39d4344f69aeec140f31
sarath <sarath@avantari.org>
2021-12-15 20:02:55 +0530

    final fixes

--- readme.md
+++ readme.md
@@ -3,3 +3,8 @@
 2. used recharts npm module for Drawing charts
 
 3. site deployed in https://romantic-saha-94fea1.netlify.app/
+
+4. home page displays all the Publishers List
+5. /publisher/:PUBLISHER page shows all the news of particular publisher in decending Order
+
+6. i tried to implement search but you didn't mention based on what search should happen
--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useCallback } from "react";
+import styled from "styled-components";
+
+import { useHistory } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const Container = styled.div`
+  margin: 2em;
+`;
+
+const Section = styled.div`
+  margin: 0 2em;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Search = () => {
-  return <div>Search ....</div>;
+  return (
+    <Container>
+      <Section>Search ...</Section>
+    </Container>
+  );
 };
 
 export default Search;
